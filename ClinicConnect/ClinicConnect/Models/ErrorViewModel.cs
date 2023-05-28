9095f682cd1ecebaf18c80c5a63a761a008f3508
chrissanchemusic <christopher.sanchez@students.mq.edu.au>
2023-05-29 09:56:18 +1000

    adding project folder

---
+++ ClinicConnect/ClinicConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicConnect.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
