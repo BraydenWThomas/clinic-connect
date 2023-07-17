b8fa73bbdafe7b212bbd04b58787f5a5816a0c40
brayden.thomas <brayden.thomas@fdmgroup.com>
2023-07-17 12:34:55 +1000

    requested changes

--- Login/LoginProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Login/LoginProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -87,7 +87,7 @@ namespace LoginProject.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
+            [EmailAddress(ErrorMessage ="Invalid email address")]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
@@ -96,7 +96,7 @@ namespace LoginProject.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "{0} must be between {2} and {1} characters", MinimumLength = 8)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -107,7 +107,7 @@ namespace LoginProject.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Passwords do not match")]
             public string ConfirmPassword { get; set; }
 
             [Required]
